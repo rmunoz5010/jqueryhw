4901de69bbf430d57aa2755ccdeb659ad2bf585c
Ramon Munoz <rgm_munoz@mac.com>
2018-03-21 16:12:51 -0400

    clean up html

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var currentScore = 0;
+var targetScore = 0;
+var winCount = 0;
+var lossCount = 0;
 var crystal = {
     blue:
     {
@@ -19,12 +23,8 @@ var crystal = {
       name: "Yellow",
       value: 0
     }
-  };
+};
 
-  var currentScore = 0;
-  var targetScore = 0;
-  var winCount = 0;
-  var lossCount = 0;
   
   
   var getRandom = function(min, max) {
@@ -34,17 +34,11 @@ var crystal = {
   var startGame = function() {
 
     currentScore = 0;
-  
-    // Set a new Target Score (between 19 and 120)
     targetScore = getRandom(19, 120);
-  
-    // Set different values for each of the crystals (between 1 and 12)
     crystal.blue.value = getRandom(1, 12);
     crystal.red.value = getRandom(1, 12);
     crystal.green.value = getRandom(1, 12);
     crystal.yellow.value = getRandom(1, 12);
-  
-    // Change the HTML to reflect all of these changes
     $("#your-score").text(currentScore);
     $("#target-score").text(targetScore);
   };
@@ -78,7 +72,6 @@ var crystal = {
   
 
   startGame();
-  
   $("#blue").click(function() {
     addValues(crystal.blue);
   });
